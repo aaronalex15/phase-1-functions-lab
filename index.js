11baeb9b44498365376ce8a4d79a7c0cffedb770
Aaron Johnson <aaronjohnson@Pauls-MBP.lan>
2024-01-30 17:27:12 -0800

    tough lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+    const headquarters = 42;
+    if (headquarters < blockNumber) {
+        return blockNumber - headquarters
+    } else {
+        return headquarters - blockNumber
+    }
+
+}
+
+function distanceFromHqInFeet(feetValue) {
+    const HqinFeet = distanceFromHqInBlocks(feetValue) * 264
+    return HqinFeet;
+
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+        return (destination - start) * 264
+    } else {
+        return (start - destination) * 264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+
+    if (distance < 400) {
+        return 0;
+    } else if (distance >= 400 && distance < 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance >= 2000 && distance < 2500) {
+        return 25;
+    } else if (distance >= 2500) {
+        return 'cannot travel that far';
+    }
+}
