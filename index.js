6c40edeed05101b16dbf11f689fd59c1fe0af67b
Aaron Johnson <aaronjohnson@Pauls-MBP.lan>
2024-03-12 18:17:52 -0700

    code complete

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function distanceFromHqInBlocks(blockNumber) {
+/* function distanceFromHqInBlocks(blockNumber) {
     const headquarters = 42;
     if (headquarters < blockNumber) {
         return blockNumber - headquarters
@@ -35,4 +35,39 @@ function calculatesFarePrice(start, destination) {
     } else if (distance >= 2500) {
         return 'cannot travel that far';
     }
+}
+*/
+
+function distanceFromHqInBlocks(numberOfBlocks) {
+const headquarters = 42
+const distance = Math.abs(headquarters - numberOfBlocks)
+return distance
+}
+
+function distanceFromHqInFeet(valueInFeet){
+const headquartersInFeet = distanceFromHqInBlocks(valueInFeet) * 264
+return headquartersInFeet
+}
+
+function distanceTravelledInFeet(start , destination){
+    if (start < destination) {
+        return (destination - start) * 264
+    }
+    if (start > destination) {
+        return (start - destination ) * 264
+    }
+}
+function calculatesFarePrice(start, destination){
+const distance = distanceTravelledInFeet(start, destination)
+
+if (distance < 400)
+return 0
+
+if (distance > 400 && distance < 2000)
+return (distance - 400) * 0.02
+
+else if (distance >= 2000 && distance <= 2500)
+return 25
+else 
+    return 'cannot travel that far';
 }
